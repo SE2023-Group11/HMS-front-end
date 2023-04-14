8f200fe8ad13fcb65fc494e62848b114729aff20
Gabriel1159 <gabriel1159@163.com>
2023-04-14 17:57:25 +0800

    zzc-0414-3

--- hms_frontend/src/components/ManagerView.vue
+++ hms_frontend/src/components/ManagerView.vue
@@ -1,16 +1,54 @@
+
 <template>
-    <TabMenu :model="items"/>
-    <router-view/>
+    <input type="radio" id="room" value="科室" v-model="notifyType">科室
+    <input type="radio" id="doctor" value="医生" v-model="notifyType">医生
+    <input type="text" v-model="name">&nbsp;
+    <button on-click="getInfo(notifyType, name)">查询</button>
+    <h1>选择是 {{ notifyType }}</h1>
+    <h1>输入信息为 {{ name }}</h1>
 </template>
 
-<script>
-    import TabMenu from 'primevue/tabmenu'
-    export default {
-        setup(){
-            let items = ['home', 'calendar']
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios'
 
-            return items
-        }
-        
+const notifyType = ref('');
+const name = ref('')
+
+function getRoomInfo(name)
+{
+    axios.post(
+        'http://localhost:8080/getRoomInfo', 
+        {roomName:name}
+        ).then((s)=>{
+            console.log(s)
+        })
+}
+
+function getDoctorInfo(name)
+{
+    axios.post(
+        'http://localhost:8080/getDoctorInfo', 
+        {doctorName:name}
+        ).then((s)=>{
+            console.log(s)
+        })
+
+}
+
+function getInfo(notifyType, name)
+{
+    if(notifyType=="科室")
+    {
+        console.log("科室")
+        getRoomInfo(name)
     }
+    else
+    {
+        console.log("医生")
+        getDoctorInfo(name)
+    }
+}
+
+
 </script>
--- hms_frontend/src/main.js
+++ hms_frontend/src/main.js
@@ -3,4 +3,5 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 
 const app = createApp(App);
+app.mount('#app');
 app.use(PrimeVue);
