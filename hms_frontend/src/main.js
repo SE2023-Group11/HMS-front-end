d96929ccfd76fd0dbd4d167b0a8b155dea9bb696
buaa21371396 <3056161580@qq.com>
2023-06-03 22:26:15 +0800

    补全main.js

--- hms_frontend/src/main.js
+++ hms_frontend/src/main.js
@@ -4,6 +4,16 @@ import PrimeVue from 'primevue/config';
 import Ripple from 'primevue/ripple';
 import router from "./router/index";
 import Dialog from 'primevue/dialog';
+import Button from "primevue/button";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   
+import Listbox from 'primevue/listbox';
+import Row from 'primevue/row';          
+import Carousel from 'primevue/carousel';
+import Card from 'primevue/card';
+import Paginator from 'primevue/paginator';
+
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
@@ -14,8 +24,18 @@ import "primevue/resources/primevue.min.css";
 
 const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
-app.use(PrimeVue)
-app.use(ConfirmationService)
-app.use(ToastService)
-app.use(router).mount('#app');
+app.use(PrimeVue);
+app.use(ConfirmationService);
+app.use(ToastService);
 app.directive('ripple', Ripple);
+app.component('Button', Button);
+app.component('Dialog', Dialog);
+app.component('DataTable',DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+app.component('Listbox',Listbox);
+app.component('Carousel',Carousel);
+app.component('Paginator',Paginator);
+app.component('Card',Card);
+app.use(router).mount('#app');
